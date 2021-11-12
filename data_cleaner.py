df21a2bfbdcfe3f6a27b3bf44a976138053c6c89
Khandoker09 <63535729+Khandoker09@users.noreply.github.com>
2021-11-12 11:04:09 +0100

    Final Version
    complete analyzer tools with out nlp

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,3 +1,12 @@
+'''
+Name :daZ
+Created by Khandoker Tanjim Ahammad
+Date: 11/11/21
+Purpose: Analyze any data sets using streamlit
+Limitations: often datasets contain too much text does not work
+
+'''
+
 import streamlit as st
 import plotly_express as px
 import pandas as pd
@@ -7,8 +16,8 @@ import io
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
 # title of the app
-st.title("Data Analyzer")
-
+st.title("daZ")
+st.title("A simple tools to analyze data and also to visulize data to find out the relation between the columns in datasets")
 # Add a sidebar
 st.sidebar.subheader("Visualization Settings")
 
@@ -16,71 +25,105 @@ st.sidebar.subheader("Visualization Settings")
 uploaded_file = st.sidebar.file_uploader(
                         label="Upload your CSV or Excel file. (200MB max)",
                          type=['csv', 'xlsx'])
-
+# Defining data set as global variable 
 global df
 if uploaded_file is not None:
     print(uploaded_file)
-    #print("hello")
-
     try:
         df = pd.read_csv(uploaded_file)
     except Exception as e:
         print(e)
         df = pd.read_excel(uploaded_file)
-
-global numeric_columns
-global non_numeric_columns
 try:
+
     # add check box to sidebar
     check_box = st.sidebar.checkbox(label="Display dataset")
-
+    # Make a tic box to decide we want to see the data or not 
     if check_box:
     # lets show the dataset
         st.write(df)
-    select = st.sidebar.selectbox('Tools', ['Describe data','Info about datasets',  'Find Missing value', 'Delete Column'], key='1')
-    # st.write(df)
-    numeric_columns = list(df.select_dtypes(['float', 'int']).columns)
-    non_numeric_columns = list(df.select_dtypes(['object']).columns)
-    non_numeric_columns.append(None)
-    print(non_numeric_columns)
-    # column filter data
-    filtered = st.multiselect("Filter columns", options=list(df.columns), default=list(df.columns))
+    # 1st Sidebar Select Box for analyze data sets 
+    select = st.sidebar.selectbox('Analyze', ['Info about datasets', 'Describe data',  'Find Missing value'])
+    #2nd sidebar-- filtering all the column 
+    filtered = st.multiselect("Overall columns", options=list(df.columns), default=list(df.columns))
     st.write(df[filtered])
-    #graph
-    chart_select= st.sidebar.selectbox(
-                    label='Select the chart type',
-                    
-                     options=['Scatterplots','Lineplot','Histogram','Funnel'])
-
-            
-    #st.sidebar.title("Analyzer",select)
-    select = st.sidebar.selectbox('Tools', ['Info about datasets', 'Describe data', 'Find Missing value', 'Add column', 'Delete Column', 'Sort datasets','Change order'], key='1')
-    
-
+    # Generate output using bunch of If else 
     if select == "Info about datasets":  
             buffer = io.StringIO()
             df.info(buf=buffer)
             s = buffer.getvalue()
             st.text(s)
-          
+    
     elif select=='Describe data':
-          
-           s=df.describe()
-           st.text(s)
-
-    elif select=='Find Missing value':
-            
-             s=df.isnull().sum()
-           
-             st.text(s)
-    # elif select=='Delete Column':
         
-    #         s= del[df['id']]      
-    #         st.text(s)
+        buffer = io.StringIO()
+        s= df.describe()
+        st.table(s)
+
+    elif select== 'Find Missing value':
+        buffer = io.StringIO()
+        s= df.isnull().sum()
+        st.table(s)
+    #3rd side bar to decide which graph to show 
+    chart_select= st.sidebar.selectbox(
+                    label='Select the chart type',
+                     options=['Scatterplots','Lineplot','Histogram','Boxplot','Barchart','Funnel'])
+    # Generate output using bunch of If else to show proper graph 
+    if chart_select == 'Scatterplots':
+        st.sidebar.subheader("Scatterplot Settings")
+        x_values = st.sidebar.selectbox('X axis', options= df.columns)
+        y_values = st.sidebar.selectbox('Y axis', options= df.columns)
+        plot = px.scatter(data_frame=df, x=x_values, y=y_values)
+        #Display the chart
+        st.plotly_chart(plot)
 
 
+    elif chart_select == 'Lineplot':
+        st.sidebar.subheader("Line Plot Settings")
+        x_values = st.sidebar.selectbox('X axis', options=df.columns )
+        y_values = st.sidebar.selectbox('Y axis', options=df.columns)
+        plot = px.line(data_frame=df, x=x_values, y=y_values)
+        st.plotly_chart(plot)
+
+    elif chart_select == 'Histogram':
+        st.sidebar.subheader("Histogram Settings")
+        x = st.sidebar.selectbox('Feature', options=df.columns)
+        bin_size = st.sidebar.slider("Number of Bins", min_value=10,
+                                     max_value=100, value=40)
+        plot = px.histogram(x=x, data_frame=df)
+        st.plotly_chart(plot)
+
+
+
+    elif chart_select == 'Boxplot':
+        st.sidebar.subheader("Boxplot Settings")
+        x = st.sidebar.selectbox("X axis", options=df.columns)
+        y = st.sidebar.selectbox("Y axis", options=df.columns)
+        plot = px.box(data_frame=df, x=x, y=y)
+        st.plotly_chart(plot)
+    
+    elif chart_select == 'Barchart':
+        st.sidebar.subheader("Barchart Settings")
+        x = st.sidebar.selectbox("X axis", options=df.columns)
+        y = st.sidebar.selectbox("Y axis", options=df.columns)
+        plot = px.bar(data_frame=df, x=x, y=y)
+        st.plotly_chart(plot)
+
+    elif chart_select == 'Funnel':
+        st.sidebar.subheader("Funnel Settings")
+        x = st.sidebar.selectbox("X axis", options=df.columns)
+        y = st.sidebar.selectbox("Y axis", options=df.columns)
+        plot = px.funnel(data_frame=df, x=x, y=y)
+        st.plotly_chart(plot)
+    elif chart_select == 'Funnel':
+        st.sidebar.subheader("Funnel Settings")
+        x = st.sidebar.selectbox("X axis", options=df.columns)
+        y = st.sidebar.selectbox("Y axis", options=df.columns)
+        plot = px.wis(data_frame=df, x=x, y=y)
+        st.plotly_chart(plot)
 
 
 except Exception as e:
     print(e)
     st.write("Please upload file to the application.")
+
